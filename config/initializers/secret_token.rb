cb34bb89fc5367cf3ac8970134a02b11632fac02
Darren Oakley <daz.oakley@gmail.com>
2013-11-30 09:20:05 +0000

    Initial commit (new rails app)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scorecard::Application.config.secret_key_base = 'd28cf03b4e3a684be51b3f44b9767b0a9a008cc717ae2963cd20ecefc147629ca80193719f35bab88a4558b460c27e4edac7120be49b61ddc5bd72223776221d'

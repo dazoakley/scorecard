a002b8d23b16bd9909ba7a1ae9e380f6033e7c2d
Darren Oakley <daz.oakley@gmail.com>
2016-02-04 21:53:38 +0000

    Document monkey patch

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,10 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Scorecard::Application.initialize!
 
-# Monkeypatch MySQL Adapter :-/
+
+# @ref: https://github.com/rails/rails/pull/13247#issuecomment-32425844
+# Monkeypatch the MySQL adapter so it works with MySQL 5.7+
+# TODO: remove once rails has been updated above 4.2
 if defined?(ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter::NATIVE_DATABASE_TYPES)
   require File.expand_path('../../lib/patches/abstract_mysql_adapter', __FILE__)
 end
